51f2574f725cfb7b4c9aed12bc5ef865e8da93b3
GreenLadyTea <kudinovamo@mer.ci.nsu.ru>
2022-05-05 16:35:08 +0700

    able to see your own profile when redirecting from users page

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function UserCard({ username }) {
+  const nick = useSelector(state => state.profile.username);
+
   return (
     <>
       <Card>
         <Card.Body>
           <Card.Text>
-            <Link to={`/people/${username}`}>{username}</Link>
+            {username === nick ? (
+              <Link to={`/profile`}>{username}</Link>
+            ) : (
+              <Link to={`/people/${username}`}>{username}</Link>
+            )}
           </Card.Text>
         </Card.Body>
       </Card>
